1d3039ac056b57e921764fcd6930ec69c942e016
kiss8981 <kdh89811@gmail.com>
2023-10-31 01:40:35 +0900

    feat: tailwind

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,25 +1,50 @@
-import * as React from 'react';
+import { classNames } from "@/utils/utils";
+import * as React from "react";
 
 type ButtonProps = {
-    isLoading?: boolean;
+  isLoading?: boolean;
+  variant?: "primary" | "danger" | "outline";
 } & React.ComponentPropsWithRef<"button">;
 
-const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
-    function Button({ children, className, isLoading, disabled, ...rest }, ref) {
-        const buttonDisabled = isLoading || disabled;
+const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(function Button(
+  { children, className, isLoading, disabled, variant, ...rest },
+  ref
+) {
+  const buttonDisabled = isLoading || disabled;
 
-        return (
-            <button
-                ref={ref}
-                type="button"
-                disabled={buttonDisabled}
-                className={className}
-                {...rest}
-            >
-                {children}
-            </button>
-        );
-    }
-);
+  return (
+    <button
+      ref={ref}
+      type="button"
+      disabled={buttonDisabled}
+      className={classNames(
+        className,
+        buttonDisabled ? "opacity-50 cursor-not-allowed" : "cursor-pointer",
+        ...[
+          variant === "primary" && [
+            "bg-primary-500 hover:bg-primary-600 text-white",
+            "focus:ring-primary-500 focus:ring-offset-primary-200 focus:ring-2 focus:ring-offset-2",
+          ],
+          variant === "danger" && [
+            "bg-red-500 hover:bg-red-600 text-white",
+            "focus:ring-red-500 focus:ring-offset-red-200 focus:ring-2 focus:ring-offset-2",
+          ],
+          variant === "outline" && [
+            "border border-gray-500 hover:border-gray-600 text-gray-500",
+            "focus:ring-gray-500 focus:ring-offset-gray-200 focus:ring-2 focus:ring-offset-2",
+          ],
+        ],
+        "rounded-md px-2 py-1 border"
+      )}
+      {...rest}
+    >
+      {children}
+    </button>
+  );
+});
+
+Button.defaultProps = {
+  variant: "primary",
+};
 
 export default Button;
--- src/components/Layouts/Header.tsx
+++ src/components/Layouts/Header.tsx
@@ -1,20 +1,34 @@
 import Image from "next/image";
+import Logo from "@/../public/logo.svg";
+import Button from "../Button";
 
 const Header = () => {
   return (
     <div className="bg-white border-b">
       <div className="flex flex-row items-center py-3 max-w-screen-2xl mx-auto px-5">
-        <Image src="/logo.svg" alt="logo" width={40} height={40} layout="responsive" className="max-w-[40px]" />
-        <span
-          style={{
-            fontFamily: "Soyo Maple",
-          }}
-          className="text-xl lg:block hidden font-bold ml-2"
-        >
-          뭐 먹으러가?
-        </span>
+        <div className="flex flex-row items-center justify-center mr-auto">
+          <Image
+            src={Logo}
+            alt="logo"
+            width={35}
+            height={35}
+            className="max-w-[40px]"
+          />
+          <span
+            style={{
+              fontFamily: "Soyo Maple",
+            }}
+            className="text-xl lg:block hidden font-bold ml-2"
+          >
+            뭐 먹으러가?
+          </span>
+        </div>
+        <div className="flex flex-row">
+          <Button variant="primary" className="mx-auto">
+            asd
+          </Button>
+        </div>
       </div>
-      
     </div>
   );
 };
